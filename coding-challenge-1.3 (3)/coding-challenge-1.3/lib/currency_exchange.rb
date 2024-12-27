139ca717cfef4cd8b5ff81740efcc56cc1b57422
Enosh Earnest <enoshsolomonn@gmail.com>
2024-12-27 17:02:59 +0000

    Code update, adding logic

--- coding-challenge-1.3 (3)/coding-challenge-1.3/lib/currency_exchange.rb
+++ coding-challenge-1.3 (3)/coding-challenge-1.3/lib/currency_exchange.rb
@@ -7,7 +7,15 @@ module CurrencyExchange
   # Raises an exception if unable to calculate requested rate.
   # Raises an exception if there is no rate for the date provided.
   def self.rate(date, from_currency, to_currency)
-    data = 
-  end
+    data = load_data
+    rates = data[date]
 
+    raise "No rates found for date: #{date}" unless rates
+    raise "Invalid currency: #{from_currency}" unless rates[from_currency]
+    raise "Invalid currency: #{to_currency}" unless rates[to_currency]
+  
+    from_rate = rates[from_currency]
+    to_rate = rates[to_currency]
+
+    (to_rate / from_rate).round(6)
 end

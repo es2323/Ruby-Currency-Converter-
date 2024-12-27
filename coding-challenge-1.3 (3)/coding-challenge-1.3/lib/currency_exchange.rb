94a8984bfff8032a49e7e0a251abdc10a7950f3a
Enosh Earnest <enoshsolomonn@gmail.com>
2024-12-27 16:42:02 +0000

    Initial commit

--- coding-challenge-1.3 (3)/coding-challenge-1.3/lib/currency_exchange.rb
+++ coding-challenge-1.3 (3)/coding-challenge-1.3/lib/currency_exchange.rb
@@ -1,10 +1,13 @@
+require "json"
+
 module CurrencyExchange
+  DATA_FILE_PATH = File.join(__dir__, "C:\Users\enosh\Desktop\CODE\RUBY\coding-challenge-1.3 (3)\coding-challenge-1.3\data\eurofxref-hist-90d.json")
 
   # Return the exchange rate between from_currency and to_currency on date as a float.
   # Raises an exception if unable to calculate requested rate.
   # Raises an exception if there is no rate for the date provided.
   def self.rate(date, from_currency, to_currency)
-    # TODO: calculate and return rate
+    data = 
   end
 
 end
